247ebd80ae8e2b3e087cc909313b2e4d6cd717d9
Floran Brutel <notFloran@users.noreply.github.com>
2021-11-12 09:15:38 +0100

    Update command to install homebrew

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ xcode-select --install
 echo "Checking for Homebrew..."
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo "Updating brew..."
